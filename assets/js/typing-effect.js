e654eab284b6c683163442ffe70a529ca2782e59
Aqua <127223292+Burhanverse@users.noreply.github.com>
2023-07-29 18:55:46 -0700

    attempt 11

--- assets/js/typing-effect.js
+++ assets/js/typing-effect.js
@@ -1,15 +1,15 @@
 // Typing Text
-var element = document.getElementById('txt-rotate');
-var toRotate = element.getAttribute('data-rotate');
-var period = element.getAttribute('data-period');
+var element = document.getElementById("txt-rotate");
+var toRotate = element.getAttribute("data-rotate");
+var period = element.getAttribute("data-period");
 
-setTimeout(function() {
+setTimeout(function () {
   new TxtRotate(element, JSON.parse(toRotate), period);
 }, 1500);
 
 function TxtRotate(element, text, period) {
   var i = 0;
-  var interval = setInterval(function() {
+  var interval = setInterval(function () {
     if (i < text.length) {
       element.innerHTML += text[i];
       i++;
@@ -18,3 +18,9 @@ function TxtRotate(element, text, period) {
     }
   }, period);
 }
+
+// INJECT CSS
+var css = document.createElement("style");
+css.type = "text/css";
+css.innerHTML = "#txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+document.body.appendChild(css);
