fe1906a7c92880fe879c62314ac632b1086f7a38
Aqua <127223292+Burhanverse@users.noreply.github.com>
2023-07-29 18:03:08 -0700

    attempt 7

---
+++ assets/js/typing-effect.js
@@ -0,0 +1,20 @@
+// Typing Text
+var element = document.getElementById('txt-rotate');
+var toRotate = element.getAttribute('data-rotate');
+var period = element.getAttribute('data-period');
+
+setTimeout(function() {
+  new TxtRotate(element, JSON.parse(toRotate), period);
+}, 1500);
+
+function TxtRotate(element, text, period) {
+  var i = 0;
+  var interval = setInterval(function() {
+    if (i < text.length) {
+      element.innerHTML += text[i];
+      i++;
+    } else {
+      clearInterval(interval);
+    }
+  }, period);
+}
