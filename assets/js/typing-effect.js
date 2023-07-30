36eeda48b338d022773aa93ae3b414233f02d6a8
Aqua <127223292+Burhanverse@users.noreply.github.com>
2023-07-30 10:58:45 -0700

    attempt 17

--- assets/js/typing-effect.js
+++ assets/js/typing-effect.js
@@ -1,56 +1,40 @@
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
+var TxtRotate = function (t, e, i) {
+  (this.toRotate = e),
+    (this.el = t),
+    (this.loopNum = 0),
+    (this.period = parseInt(i, 10) || 2e3),
+    (this.txt = ""),
+    this.tick(),
+    (this.isDeleting = !1);
 };
+function randomizeOrder() {
+  for (
+    var t = document.getElementById("skills"),
+      e = t.getElementsByTagName("div"),
+      i = document.createDocumentFragment();
+    e.length;
 
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-  var that = this;
-  var delta = 300 - Math.random() * 100;
-
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
+  )
+    i.appendChild(e[Math.floor(Math.random() * e.length)]);
+  t.appendChild(i);
+}
+TxtRotate.prototype.tick = function () {
+  var t = this.loopNum % this.toRotate.length,
+    t = this.toRotate[t],
+    e =
+      (this.isDeleting
+        ? (this.txt = t.substring(0, this.txt.length - 1))
+        : (this.txt = t.substring(0, this.txt.length + 1)),
+      (this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>"),
+      this),
+    i = 200 - 100 * Math.random();
+  this.isDeleting && (i /= 5),
+    this.isDeleting || this.txt !== t
+      ? this.isDeleting &&
+        "" === this.txt &&
+        ((this.isDeleting = !1), this.loopNum++, (i = 500))
+      : ((i = this.period), (this.isDeleting = !0)),
+    setTimeout(function () {
+      e.tick();
+    }, i);
 };
